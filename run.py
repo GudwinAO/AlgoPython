b064202d782a006c5649985a2fc6774f8fb6e86a
Aleksandr <gudwinao88@gmail.ru>
2023-03-09 15:12:05 +0400

    Lesson_5_final

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from wsgiref.simple_server import make_server
-from simba_framework.main import Framework
+from simba_framework.main import Framework, DebugApplication, FakeApplication
 from urls import fronts
 from views import routes
 
 
 application = Framework(routes, fronts)
+#application = DebugApplication(routes, fronts)
+#application = FakeApplication(routes, fronts)
+
+
 
 with make_server('', 8080, application) as httpd:
     print("Запуск на порту 8080...")
